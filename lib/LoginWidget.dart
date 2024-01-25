e5b71e241e98e5de5b545b3246416510464f9c9a
Sad060 <105322767+Sad060@users.noreply.github.com>
2024-01-25 17:13:03 +0200

    Update LoginWidget.dart

--- lib/LoginWidget.dart
+++ lib/LoginWidget.dart
@@ -1,63 +1,106 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'RegistrationWidget.dart'; // Assuming RegistrationWidget is defined in auth.dart
 
-class LoginWidget extends StatefulWidget {
-  const LoginWidget({Key? key}) : super(key: key);
+class RegistrationLoginWidget extends StatefulWidget {
+  const RegistrationLoginWidget({Key? key}) : super(key: key);
 
   @override
-  _LoginWidgetState createState() => _LoginWidgetState();
+  _RegistrationLoginWidgetState createState() =>
+      _RegistrationLoginWidgetState();
 }
 
-class _LoginWidgetState extends State<LoginWidget> {
-  late TextEditingController _emailController, _passwordController;
-  late FocusNode _emailFocusNode, _passwordFocusNode;
+class _RegistrationLoginWidgetState extends State<RegistrationLoginWidget> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   late SharedPreferences _prefs;
-  late bool _passwordVisibility; // Add this line
 
   @override
   void initState() {
     super.initState();
-    _emailController = TextEditingController();
-    _passwordController = TextEditingController();
-    _emailFocusNode = FocusNode();
-    _passwordFocusNode = FocusNode();
-    _passwordVisibility = false; // Initialize _passwordVisibility
-    _loadSharedPreferences();
+    _loadCredentials();
   }
 
-  Future<void> _loadSharedPreferences() async {
+  Future<void> _loadCredentials() async {
     _prefs = await SharedPreferences.getInstance();
   }
 
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    _emailFocusNode.dispose();
-    _passwordFocusNode.dispose();
-    super.dispose();
-  }
+  void _register() {
+    String email = _emailController.text;
+    String password = _passwordController.text;
 
-  bool _validateCredentials() {
-    // Retrieve stored email and password from SharedPreferences
-    String? savedEmail = _prefs.getString('email');
-    String? savedPassword = _prefs.getString('password');
-
-    // Compare with entered credentials
-    return savedEmail == _emailController.text && savedPassword == _passwordController.text;
-  }
-
-  void _handleLogin() {
-    if (_validateCredentials()) {
-      // Credentials are correct, proceed with login logic
-      print('Login successful!');
-    } else {
-      // Credentials are incorrect, show an error message
+    if (_prefs.getKeys().contains(email)) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text('Invalid credentials. Please try again.'),
+          content: Text('This email is already in use'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    } else {
+      _prefs.setString(email, password);
+
+      _emailController.clear();
+      _passwordController.clear();
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Account created successfully'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    }
+  }
+
+  void _login() {
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    String? storedEmail = _prefs.getString('email');
+    String? storedPassword = _prefs.getString('password');
+
+    if (email == storedEmail && password == storedPassword) {
+      // Credentials are correct, allow login
+      // Your further logic for successful login
+      print('Login successful');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Login successful'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    } else {
+      // Credentials are incorrect
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Invalid credentials'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+      print('Login failed');
+    }
+
+    // Clear text fields
+    _emailController.clear();
+    _passwordController.clear();
+  }
+
+  void _changePassword() {
+    String email = _emailController.text;
+    String newPassword = _passwordController.text;
+
+    // Проверяем, существует ли пользователь с таким email
+    if (_prefs.getKeys().contains(email)) {
+      _prefs.setString(email, newPassword); // Изменяем пароль
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Password changed successfully'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('User is not found'),
           duration: Duration(seconds: 2),
         ),
       );
@@ -66,203 +109,68 @@ class _LoginWidgetState extends State<LoginWidget> {
 
   @override
   Widget build(BuildContext context) {
-    if (Theme.of(context).platform == TargetPlatform.iOS) {
-      SystemChrome.setSystemUIOverlayStyle(
-        SystemUiOverlayStyle(
-          statusBarBrightness: Theme.of(context).brightness,
-          systemStatusBarContrastEnforced: true,
-        ),
-      );
-    }
-
-    return GestureDetector(
-      onTap: () {
-        if (_emailFocusNode.canRequestFocus ||
-            _passwordFocusNode.canRequestFocus) {
-          FocusScope.of(context).requestFocus(FocusNode());
-        }
-      },
-      child: Scaffold(
-        key: GlobalKey<ScaffoldState>(),
-        backgroundColor: Colors.white,
-        body: Container(
-          height: double.infinity,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Color(0xFF4B39EF), Color(0xFFEE8B60)],
-              stops: [0, 1],
-              begin: AlignmentDirectional(0.87, -1),
-              end: AlignmentDirectional(-0.87, 1),
-            ),
-          ),
-          alignment: AlignmentDirectional(0, -1),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(0, 70, 0, 16),
-                  child: Container(
-                    width: 200,
-                    height: 70,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                    alignment: AlignmentDirectional(0, 0),
-                    child: Align(
-                      alignment: AlignmentDirectional(-1, 0),
-                      child: Text(
-                        'Memento Mori',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontFamily: 'Plus Jakarta Sans',
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                AnimatedContainer(
-                  duration: Duration(milliseconds: 300),
-                  curve: Curves.easeInOut,
-                  transform: Matrix4.translationValues(0, 70, 0),
-                  child: Align(
-                    alignment: AlignmentDirectional(0, 0),
-                    child: Padding(
-                      padding: EdgeInsets.all(16),
-                      child: Container(
-                        width: double.infinity,
-                        constraints: BoxConstraints(maxWidth: 570),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              blurRadius: 4,
-                              color: Color(0x33000000),
-                              offset: Offset(0, 2),
-                            )
-                          ],
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.max,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(
-                              'Welcome Back',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontFamily: 'Plus Jakarta Sans',
-                                color: Color(0xFF101213),
-                                fontSize: 36,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            Padding(
-                              padding:
-                                  EdgeInsetsDirectional.fromSTEB(0, 12, 0, 24),
-                              child: Text(
-                                'Fill out the information below to access your account.',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontFamily: 'Plus Jakarta Sans',
-                                  color: Color(0xFF57636C),
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ),
-                            for (var inputField in [
-                              _buildInputField('Email', _emailController,
-                                  _emailFocusNode, Icons.email),
-                              _buildInputField('Password', _passwordController,
-                                  _passwordFocusNode, Icons.lock,
-                                  obscureText: !_passwordVisibility),
-                            ])
-                              Container(
-                                width: double.infinity,
-                                child: inputField,
-                              ),
-                            Padding(
-                              padding:
-                                  EdgeInsetsDirectional.fromSTEB(10, 8, 10, 8),
-                              child: ElevatedButton(
-                                onPressed: _handleLogin,
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.blue,
-                                  textStyle: TextStyle(color: Colors.white),
-                                ),
-                                child: Text('Login'),
-                              ),
-                            ),
-                            Padding(
-                              padding:
-                                  EdgeInsetsDirectional.fromSTEB(10, 8, 10, 8),
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => RegistrationWidget(),
-                                    ),
-                                  );
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.blue,
-                                  textStyle: TextStyle(color: Colors.white),
-                                ),
-                                child: Text('Register'),
-                              ),
-                            ),
-                            // Hidden button
-                            Visibility(
-                              visible: true,
-                              child: Padding(
-                                padding:
-                                    EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
-                                child: ElevatedButton(
-                                  onPressed: () {
-                                    // Handle hidden button press
-                                    print('Hidden button pressed!');
-                                  },
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: Colors.white,
-                                    textStyle: TextStyle(color: Colors.white),
-                                  ),
-                                  child: Text('Hidden Button'),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Memento Mori'), // Название добавлено
       ),
-    );
-  }
-
-  Widget _buildInputField(String label, TextEditingController controller,
-      FocusNode focusNode, IconData icon,
-      {bool obscureText = false}) {
-    return Padding(
-      padding: EdgeInsetsDirectional.fromSTEB(16, 8, 16, 8),
-      child: TextFormField(
-        controller: controller,
-        focusNode: focusNode,
-        obscureText: obscureText,
-        decoration: InputDecoration(
-          labelText: label,
-          prefixIcon: Icon(icon),
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+      body: Container(
+        // Добавлен контейнер для фона с градиентом
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color.fromARGB(255, 210, 16, 228),
+                Color.fromARGB(255, 182, 13, 13)
+              ]),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue),
+                  ),
+                ),
+              ),
+              SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: _register,
+                    child: Text('Register'),
+                  ),
+                  ElevatedButton(
+                    onPressed: _login,
+                    child: Text('Login'),
+                  ),
+                  ElevatedButton(
+                    onPressed: _changePassword,
+                    child: Text('Change password'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
