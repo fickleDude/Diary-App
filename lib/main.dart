0b5c198e8f68f7989574747bf154101c5ca3fc8f
User <User@DESKTOP-96SKBJV>
2024-01-27 13:25:56 +0300

    Without list

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-import 'UI/login_page.dart';
+import 'UI/new_note_page.dart';
+import 'UI/welcome_page.dart';
 
 void main() async {
   // initialize firebase
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp().then((value)
-    => print(value.options.projectId));
+  //await Firebase.initializeApp(
+  //  // Replace with actual values
+  //  options: FirebaseOptions(
+  //    apiKey: "AIzaSyD84979B0rrV1Bqsq5vcH7C8D2tlbPHqGs",
+  //    appId: "1:661084623098:android:fe62484a95066590967943",
+  //    messagingSenderId: "661084623098",
+  //    projectId: "diary-1b0c3",
+  //  ),
+  //);
   runApp(MyApp());
 }
 
@@ -36,7 +43,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LoginPage(),
+      home: New_note_Page(),
     );
   }
 }
